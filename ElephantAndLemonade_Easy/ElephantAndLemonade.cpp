0e47b6522c5dcc50a9699cd1e34b5b999cc61047
Abhishek Bansal <discover.ab@gmail.com>
2014-07-13 00:35:23 +0530

    + adding two more solved problems (climbing stairs not yet done)

---
+++ ElephantAndLemonade_Easy/ElephantAndLemonade.cpp
@@ -0,0 +1,134 @@
+﻿/* 
+http://www.codechef.com/problems/LELEMON
+
+Little Elephant likes lemonade.
+When Little Elephant visits any room, he finds the bottle of the lemonade in that room that contains the 
+greatest number of litres of lemonade and drinks it all.
+There are n rooms (numbered from 0 to n-1), each contains Ci bottles. Each bottle has a volume (in litres). 
+The first room visited by Little Elephant was P0-th, the second - P1-th, ..., the m-th - Pm-1-th room.
+Note that Little Elephant may visit a room more than once.
+
+Find for Little Elephant the total volume of lemonade he has drunk.
+
+Input
+First line of the input contains single integer T - the number of test cases. T test cases follow. 
+First line of each test case contains pair of integers n and m. Second line contains m integers separated by 
+a single space - array P. Next n lines describe bottles in each room in such format: Ci V0 V1 ... VCi-1, 
+where V is the list of volumes (in liters) of all bottles in i-th room.
+
+Output
+In T lines print T integers - the answers for the corresponding test cases.
+
+Constraints
+1 <= T <= 10
+1 <= n, Ci <= 100
+1 <= m <= 10^4
+0 <= Pi < n
+1 <= Vi <= 10^5
+
+Example
+
+Input:
+2
+3 3
+0 2 1
+3 1 2 3
+1 7
+2 4 7
+4 7
+0 1 3 0 1 0 0
+1 7
+3 9 4 5
+7 1 2 3 4 5 6 7
+1 1
+
+Output:
+17
+22
+*/
+
+#include <iostream>
+#include <stdio.h>
+
+#define TEST 1
+
+int main()
+{
+#if TEST
+    // open file to read it like stdin
+    freopen("input.txt", "r", stdin);
+
+    // open file to use as stdout
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // read values from file
+    int noOfCases;
+    scanf("%d", &noOfCases);
+
+    while (noOfCases > 0)
+    {
+        int noOfRooms, m;
+        scanf("%d", &noOfRooms);
+        scanf("%d", &m);
+
+        // the P array
+        int* visitOrder = new int[m];
+        for(int i = 0; i < m; i++)
+        {
+            scanf("%d", &visitOrder[i]);
+        }
+
+        // lets create 2D matrix for bottles and rooms
+        // at mat[i][0] we will store no of bottles in ith room and then volume in respective bottles in rest of array
+        int **bottleVolMat = new int*[noOfRooms];
+
+        for(int i = 0; i < noOfRooms; i++)
+        {
+            // read ci
+            int noOfBottles = 0;
+            scanf("%d", &noOfBottles);
+
+            bottleVolMat[i] = new int[noOfBottles + 1];
+            bottleVolMat[i][0] = noOfBottles;
+            for(int j = 1; j <= noOfBottles; j++)
+            {
+                scanf("%d", &bottleVolMat[i][j]);
+            }
+        }
+
+        // now lets start visiting and collecting lemonade 
+        // for our Little Elephant
+        unsigned long result = 0;
+        for(int i = 0; i < m; i++)
+        {
+            int room = visitOrder[i];
+            int noOfBottles = bottleVolMat[room][0];
+            // drink max vol bottle in this room 
+            // we have bottles in bottleVolMat
+            unsigned int maxVol = 0, index = 1;
+            for(int j = 1; j <= noOfBottles; j++)
+            {
+                if(bottleVolMat[room][j] > maxVol)
+                {
+                    maxVol = bottleVolMat[room][j];
+                    index = j;
+                }
+            }
+            result += maxVol;
+            // mark volume as zero as its already in stomach :D
+            bottleVolMat[room][index] = 0;
+        }
+
+        printf("%d\n", result);
+
+        --noOfCases;
+        delete [] visitOrder;
+
+        for(int i = 0; i < noOfRooms; i++)
+        {
+            delete [] bottleVolMat[i];
+        }
+        delete [] bottleVolMat;
+    }
+}
---
+++ LackOfLogic_Easy/LackOfLogic.cpp
@@ -0,0 +1,133 @@
+﻿/*
+http://www.codechef.com/problems/NOLOGIC
+
+Consider the following dialog.
+- Father, what is the lack of logic?
+- Bunny.
+- Where?
+- Day.
+
+As you see, father's answers have no logic demonstrating the son the notion of "lack of logic" using live example :D
+Let's try to formalize things. Note that both father's answers have no common letters with the corresponding questions.
+Probably this is the reason. Let's say that the answer has no logic if it has no common letters with the question. 
+Your task is to write a bot that will answer annoying comments of contestants at codechef using this definition. 
+Note that letter denotes a lowercase or uppercase English alphabet (ASCII code from 65 to 90 or from 97 to 122).
+Namely, you will be given several questions and you need to produce answers to these questions that have no logic. 
+Note that lowercase and uppercase letters are considered the same when treating "no common letters" rule. Also note
+that this rule concerns only letters. The answer should contain at least one letter. If it is impossible to produce 
+such answer then output "~" (tilde sign, quotes are for clarity).
+
+Input
+The first line of the input contains an integer T denoting the number of test cases. The description of T test cases
+follows. The only line of each test case contains the question. The question is a non-empty string where each character
+has ASCII code from 32 to 126, inclusive (refer to this).
+
+Output
+For each test case, output a single line containing the answer to this question that has no logic or "~" (quotes for 
+clarity) if it is impossible to find such answer. Each character in the answer should be either a space (ASCII code 32), 
+or the letter of English alphabet (lowercase or uppercase). Recall that the answer should contain at least one letter. 
+The number of characters in the answer should not exceed 314. You can output any answer satisfying these constraints 
+that has no logic.
+
+Note! Special judge is very strict for this problem and check your output character-by-character. Be sure that you follow 
+the mentioned output format precisely. For instance, your output should contain exactly T new-line characters (ASCII code 10)
+- one after each answer.
+
+Constraints
+1 ≤ T ≤ 314
+1 ≤ the number of characters in the question ≤ 314
+Example
+
+Input:
+4
+Father, what is the lack of logic?
+Where?
+The quick brown fox jumps over the lazy dog.
+!!!
+
+Output:
+Bunny
+Day
+~
+You Can output aLoMsT anytihng HERE
+
+Explanation
+Example cases 1 and 2 correspond to the dialog in the problem statement. Note that we don't print dots since it is allowed
+to have only spaces and letters in the answer.
+In example case 3 each letter of English alphabet is used in the question. Hence we can't produce the answer that has no logic.
+
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+#define TEST 1
+
+char alphabets[26];
+char ques[315];
+
+int main()
+{
+#if TEST
+    // open file to read it like stdin
+    freopen("input.txt", "r", stdin);
+
+    // open file to use as stdout
+    freopen("output.txt", "w", stdout);
+#endif
+
+    // read values from file
+    unsigned int nTC, count = 0;
+
+    scanf("%d", &nTC);
+    // ignore a line end
+    getchar();
+    while (0 < nTC)
+    {
+        int noOfBytes = 314;
+        std::string curQues;
+        std::getline(std::cin, curQues);
+        int len = curQues.length();
+
+        // reset alphabets array
+        for(int i = 0; i < 26; i++)
+        {
+            alphabets[i] = 0;
+        }
+
+        // track what all alphabets came i guess we can ignore all other 
+        // characters (fingerscrossed)
+        for(int i = 0; i < curQues.length(); i++)
+        {
+            if(curQues[i] >= 65 && curQues[i] <= 90)
+            {
+                alphabets[curQues[i] - 65] = 1;
+            }
+            else if(curQues[i] >= 97 && curQues[i] <= 122)
+            {
+                alphabets[curQues[i] - 97] = 1;
+            }
+        }
+
+        // now print whatever not came
+        // a boolean to keep tilde track
+        bool isTilde = true;
+        for(int i = 0; i < 26; i++)
+        {
+            if(!alphabets[i])
+            {
+                putchar(i + 65);
+                isTilde = false;
+            }
+        }
+
+        if(isTilde)
+            putchar('~');
+
+        printf("\n");
+
+        --nTC;
+    }
+    return 0;
+}
